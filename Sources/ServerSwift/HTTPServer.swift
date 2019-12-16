1dcd09834c40d1c4708e3fc7167846756b43050e
fummicc1 <fumiya.tennis1@gmail.com>
2019-12-16 14:48:59 +0900

    setup_socket && add resource(ref) folder.

--- Sources/ServerSwift/HTTPServer.swift
+++ Sources/ServerSwift/HTTPServer.swift
@@ -1,3 +1,15 @@
-class HTTPServer {
+protocol Server {
+    var port: Int? { get }
+    var address: String? { get }
     
+    func listen(on port: Int?, address: String?) throws
+}
+
+class HTTPServer: Server {
+    var port: Int?
+    var address: String?
+    
+    func listen(on port: Int?, address: String?) throws {
+        
+    }
 }
--- Tests/ServerSwiftTests/ServerSwiftTests.swift
+++ Tests/ServerSwiftTests/ServerSwiftTests.swift
@@ -2,14 +2,13 @@ import XCTest
 @testable import ServerSwift
 
 final class ServerSwiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ServerSwift().text, "Hello, World!")
+    
+    func testTCPConnection() {
+        let server = HTTP.createServer()
+        try? server.listen(on: 8080, address: "localhost")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testTCPConnection),
     ]
 }
